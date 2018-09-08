cf21aefa24024eb53edd87d96b40cbfd2d8bab16
splitt3r <splitt3r@users.noreply.github.com>
2018-09-08 04:19:23 +0200

    Added refresh

--- frontend/src/components/Map.vue
+++ frontend/src/components/Map.vue
@@ -48,7 +48,8 @@ export default {
     latlng: {
       lat: 0,
       lng: 0
-    }
+    },
+    map: null
   }),
   computed: {
     username () {
@@ -59,59 +60,23 @@ export default {
     }
   },
   mounted () {
-    const mymap = L.map('mapid').setView([51.618, 1.496], 10)
+    this.map = L.map('mapid').setView([51.618, 1.496], 10)
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors',
       maxZoom: 18
-    }).addTo(mymap)
+    }).addTo(this.map)
 
-    const LeafIcon = L.Icon.extend({
-      options: {
-        shadowUrl: '',
-        iconSize: [ 38, 95 ],
-        shadowSize: [ 0, 0 ],
-        iconAnchor: [ 22, 94 ],
-        shadowAnchor: [ 0, 0 ],
-        popupAnchor: [ -3, -76 ]
-      }
-    })
-
-    axios.get('http://localhost:8080/cleaner/all')
-      .then(response => {
-        response.data.forEach(vessel => {
-          const icon = new LeafIcon({ iconUrl: vessel.iconUrl })
-          L.marker([ vessel.lat, vessel.lng ], { icon: icon }).addTo(mymap).bindPopup(`
-          <p><a href="${vessel.companyUrl}" target="_blank">${vessel.name}</a></p>
-          <p>Type: ${vessel.type}</p>
-          <p>Category: ${vessel.category}</p>
-          <a href="${vessel.diagramUrl}" target="_blank"><img src="${vessel.diagramUrl}"></a>
-        `).openPopup()
-        })
-      })
-      .catch(error => console.log(error))
-
-    axios.get('http://localhost:8080/pollutionSpot/all')
-      .then(response => {
-        response.data.forEach(report => {
-          L.marker([ report.lat, report.lng ]).addTo(mymap).bindPopup(`
-          <p>Author: ${report.author.name}</p>
-          <p>Category: ${report.category}</p>
-          <a href="${report.imageUrl}" target="_blank"><img src="${report.imageUrl}"></a>
-        `).openPopup()
-        })
-      })
-      .catch(error => console.log(error))
-
-    mymap.on('click', e => {
+    this.map.on('click', e => {
       this.latlng = e.latlng
       this.showDialog = true
     })
+
+    this.refresh()
   },
   methods: {
     save () {
       this.showDialog = false
-      store.commit('increment')
       axios.post('http://localhost:8080/pollutionSpot/add', {
         author: { id: 1, email: this.email, name: this.username },
         category: this.category,
@@ -119,7 +84,48 @@ export default {
         lng: this.latlng.lng,
         imageUrl: 'https://public-media.smithsonianmag.com/filer/4b/93/4b93429f-9241-4653-adb3-65ecfc7eaa5a/istock_20210548_medium.jpg'
       })
-        .then(response => console.log(response))
+        .then(response => {
+          store.commit('increment')
+          this.refresh()
+        })
+        .catch(error => console.log(error))
+    },
+    refresh () {
+      const LeafIcon = L.Icon.extend({
+        options: {
+          shadowUrl: '',
+          iconSize: [ 38, 95 ],
+          shadowSize: [ 0, 0 ],
+          iconAnchor: [ 22, 94 ],
+          shadowAnchor: [ 0, 0 ],
+          popupAnchor: [ -3, -76 ]
+        }
+      })
+
+      axios.get('http://localhost:8080/cleaner/all')
+        .then(response => {
+          response.data.forEach(vessel => {
+            const icon = new LeafIcon({ iconUrl: vessel.iconUrl })
+            L.marker([ vessel.lat, vessel.lng ], { icon: icon }).addTo(this.map).bindPopup(`
+          <p><a href="${vessel.companyUrl}" target="_blank">${vessel.name}</a></p>
+          <p>Type: ${vessel.type}</p>
+          <p>Category: ${vessel.category}</p>
+          <a href="${vessel.diagramUrl}" target="_blank"><img src="${vessel.diagramUrl}"></a>
+        `).openPopup()
+          })
+        })
+        .catch(error => console.log(error))
+
+      axios.get('http://localhost:8080/pollutionSpot/all')
+        .then(response => {
+          response.data.forEach(report => {
+            L.marker([ report.lat, report.lng ]).addTo(this.map).bindPopup(`
+          <p>Author: ${report.author.name}</p>
+          <p>Category: ${report.category}</p>
+          <a href="${report.imageUrl}" target="_blank"><img src="${report.imageUrl}"></a>
+        `).openPopup()
+          })
+        })
         .catch(error => console.log(error))
     }
   }
