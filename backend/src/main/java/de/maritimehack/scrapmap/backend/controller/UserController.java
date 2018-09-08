b123502d05a3dd7a6024bf546ee8bd74a639a156
Christoph Weinzierl <christoph.weinzierl@posteo.de>
2018-09-08 04:04:30 +0200

    add xp value to user

--- backend/src/main/java/de/maritimehack/scrapmap/backend/controller/UserController.java
+++ backend/src/main/java/de/maritimehack/scrapmap/backend/controller/UserController.java
@@ -7,12 +7,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller    // This means that this class is a Controller
-@RequestMapping(path = "/customer") // This means URL's start with /scrapmap (after Application path)
+@Controller
+@RequestMapping(path = "/customer")
 public class UserController {
     @Autowired
     private UserRepository userRepository;
@@ -20,14 +21,8 @@ public class UserController {
     @CrossOrigin(origins = "http://localhost:8000")
     @PostMapping(path = "/add") // Map ONLY GET Requests
     public @ResponseBody
-    String addNewUser(@RequestParam String name, @RequestParam String email) {
-        // @ResponseBody means the returned String is the response, not a view name
-        // @RequestParam means it is a parameter from the GET or POST request
-
-        User n = new User();
-        n.setName(name);
-        n.setEmail(email);
-        userRepository.save(n);
+    String addNewUser(@RequestBody User user) {
+        userRepository.save(user);
         return "Saved";
     }
 
--- backend/src/main/java/de/maritimehack/scrapmap/backend/entities/User.java
+++ backend/src/main/java/de/maritimehack/scrapmap/backend/entities/User.java
@@ -15,6 +15,8 @@ public class User {
 
     private String email;
 
+    private Long xp;
+
     public Integer getId() {
         return id;
     }
@@ -37,5 +39,13 @@ public class User {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public Long getXp() {
+        return xp;
+    }
+
+    public void setXp(Long xp) {
+        this.xp = xp;
     }
 }
